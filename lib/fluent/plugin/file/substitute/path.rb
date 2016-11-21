58f24fd0cde9ae9d5d7fa4744eb193f8245b41d1
Masayasu Yamamoto <m2yamamoto@gmail.com>
2016-11-21 23:51:01 +0900

    $ bundle gem fluent-plugin-file-substitute-path

---
+++ lib/fluent/plugin/file/substitute/path.rb
@@ -0,0 +1,13 @@
+require "fluent/plugin/file/substitute/path/version"
+
+module Fluent
+  module Plugin
+    module File
+      module Substitute
+        module Path
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
---
+++ spec/fluent/plugin/file/substitute/path_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Fluent::Plugin::File::Substitute::Path do
+  it "has a version number" do
+    expect(Fluent::Plugin::File::Substitute::Path::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
